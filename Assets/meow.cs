77fc19f5c4831f65bd9f7e6f3bd71d6c96d33f0e
Aimee Sullivan <sullivai@oregonstate.edu>
2019-01-21 19:30:08 -0500

    aimee2 branch

---
+++ Assets/meow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meow : MonoBehaviour
+{
+
+     public SpriteRenderer sayMeow;
+     public int delay = 100;
+     int counter;
+     bool toggle = false;
+
+     // Start is called before the first frame update
+     void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          Flash(sayMeow);
+     }
+
+     void Flash(SpriteRenderer sr) {
+          if (counter >= delay)
+          {
+               counter = 0;
+               toggle = !toggle;
+               if (toggle)
+               {
+                    sr.enabled = true;
+               }
+               else {
+                    sr.enabled = false;
+               }
+          }
+          else {
+               counter++;
+          }
+     }
+
+
+
+
+
+
+
+
+}
