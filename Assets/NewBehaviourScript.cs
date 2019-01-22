474872ecf9b8cde671969ec356e5ab18f71f8227
sullivai <sullivai@oregonstate.edu>
2019-01-21 19:17:43 -0500

    trying branch

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-          Debug.Log("THis is a test");
+          Debug.Log("This is a test in the new branch.");
     }
 
     // Update is called once per frame
